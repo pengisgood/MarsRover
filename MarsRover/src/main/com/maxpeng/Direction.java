a80173ec17a5000d9a16240535362d3320252769
pengisgood <pengisgood@gmail.com>
2013-03-23 13:04:08 +0800

    refactor enum Dirction

--- MarsRover/src/main/com/maxpeng/Direction.java
+++ MarsRover/src/main/com/maxpeng/Direction.java
@@ -10,37 +10,7 @@ import java.util.Arrays;
  * version: 0.1
  */
 public enum Direction {
+    EAST, SOUTH, WEST, NORTH;
 
-    EAST('E') {
-        public char convertToChar() {
-            return 'E';
-        }
-    },
-    SOUTH('S') {
-        public char convertToChar() {
-            return 'S';
-        }
-    },
-    WEST('W') {
-        public char convertToChar() {
-            return 'W';
-        }
-    },
-    NORTH('N') {
-        public char convertToChar() {
-            return 'N';
-        }
-    };
-
-
-    abstract public char convertToChar();
-
-    private char direction;
     public static final ArrayList DIRECTIONS = new ArrayList(Arrays.asList(EAST, SOUTH, WEST, NORTH));
-
-
-    Direction(char direction) {
-        this.direction = direction;
-    }
-
 }
--- README.md
+++
@@ -1,4 +0,0 @@
-MarsRover
-=========
-
-TDD practice
