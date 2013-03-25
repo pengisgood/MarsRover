e4db315eba899181cd41fcf58377cfb67d4f7586
pengisgood <pengisgood@gmail.com>
2013-03-25 18:10:01 +0800

    most simple implementation.

--- src/main/java/com/maxpeng/Application.java
+++ src/main/java/com/maxpeng/Application.java
@@ -8,6 +8,7 @@ package com.maxpeng;
  * version: 0.1
  */
 public class Application {
+
     private final Rover rover;
 
     public Application(Rover rover) {
@@ -15,8 +16,19 @@ public class Application {
     }
 
     public void receiveCommand(char commandLetter) {
-        Command command = CommandFactory.buildCommand(commandLetter, rover);
-        command.execute();
+
+        if (commandLetter == 'L') {
+            rover.turnLeft();
+        }
+
+        if (commandLetter == 'R') {
+            rover.turnRight();
+        }
+
+        if (commandLetter == 'M') {
+            rover.move();
+        }
+
     }
 
 }
